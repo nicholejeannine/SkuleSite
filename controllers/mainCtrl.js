bc9355330c14df50f5e893f4c09292a205ca7a0a
nicholejeannine <nicholejeannine@gmail.com>
2015-04-15 03:02:40 -0700

    oops. again.

--- controllers/mainCtrl.js
+++ controllers/mainCtrl.js
@@ -3,23 +3,31 @@ var router = express.Router();
 
 // get request made to home page. Response: "here's your generic home page."
 router.get('/', function (req, res) {
-	res.render('main/index');
+	res.render('main/index', ({
+		signedIn: req.isAuthenticated(),
+	}));
 });
 
 //GET /about  page is requested, respond with the static "about" page.
 router.get('/about', function (req, res) {
-	res.render('main/about');
+	res.render('main/index', ({
+		signedIn: req.isAuthenticated(),
+	}));
 });
 
 // signin page is requested, respond with the signin page.
 router.get('/signin', function (req, res) {
-	res.render('main/signin');
+	res.render('main/index', ({
+		signedIn: req.isAuthenticated(),
+	}));
 });
 
 
 //sign-up page is requested, respond with the signup page.
 router.get('/signup', function (req, res) {
-	res.render('main/signup');
+	res.render('main/index', ({
+		signedIn: req.isAuthenticated(),
+	}));
 });
 
 
